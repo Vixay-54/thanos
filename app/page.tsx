c3c6f1990517766a47d56770f4131bb04a3a05b8
Vixay-54 <vixayvixay25@gmail.com>
2025-06-27 02:47:35 -0700

    test

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Hello from Next.js!</h1>
+      <p>This is a simple site deployed to Vercel 🚀</p>
+    </main>
+  );}
